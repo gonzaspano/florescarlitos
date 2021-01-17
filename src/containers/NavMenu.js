b6a6f000e0e96e60e074cf6389a123a7192488b9
Gonzalo Spano <gonzaspano@gmail.com>
2021-01-17 18:10:55 -0300

    ajustes

--- src/containers/NavMenu.js
+++ src/containers/NavMenu.js
@@ -15,15 +15,15 @@ export default function NavMenu() {
         <div bg="light" variant="dark" className="navbar-container" >
           <div className="categories-container">
             <NavDropdown className="text text-dropdown" title="Categorías" id="nav-dropdown">
-              <NavDropdown.Item className="text-dropdown" eventKey="4.1"><Link to="/categories/mates"> Mates </Link></NavDropdown.Item>
-              <NavDropdown.Item className="text-dropdown" eventKey="4.2"><Link to="/categories/bombillas">Bombillas</Link></NavDropdown.Item>
-              <NavDropdown.Item className="text-dropdown" eventKey="4.3"><Link to="/categories/materas"> Materas</Link></NavDropdown.Item>
-              <NavDropdown.Item className="text-dropdown" eventKey="4.3"><Link to="/categories/yerberas"> Yerberas </Link></NavDropdown.Item>            
+              <NavDropdown.Item className="text-dropdown" eventKey="4.1"><Link className="text-link-title" to="/categories/mates"> Mates </Link></NavDropdown.Item>
+              <NavDropdown.Item className="text-dropdown" eventKey="4.2"><Link className="text-link-title" to="/categories/bombillas">Bombillas</Link></NavDropdown.Item>
+              <NavDropdown.Item className="text-dropdown" eventKey="4.3"><Link className="text-link-title" to="/categories/materas"> Materas</Link></NavDropdown.Item>
+              <NavDropdown.Item className="text-dropdown" eventKey="4.3"><Link className="text-link-title" to="/categories/yerberas"> Yerberas </Link></NavDropdown.Item>            
             </NavDropdown>
-            <Link to="/addproducts" className="text"> <p className="text"> Nuevo producto </p></Link>
+            <Link to="/addproducts" className="text text-link-title"> <p className="text"> Nuevo producto </p></Link>
           </div>
           <div className="title-container">
-            <Link to="/">  <h1 className="text"> TODO MATE</h1></Link>
+            <Link className="text-link-title" to="/">  <h1 className="text"> TODO MATE</h1></Link>
           </div>
           <div className="icons-container">
             <CartIcon displayCart={() => setModalShow(true)} />
